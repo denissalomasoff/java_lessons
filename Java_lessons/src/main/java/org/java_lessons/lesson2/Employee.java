966e42fe8a8d12d4c16f3f73033b6682f24ba4a1
Денис_Денис <denissalomasoff@gmail.com>
2026-01-29 19:33:58 +0300

    Урок 2

---
+++ Java_lessons/src/main/java/org/java_lessons/lesson2/Employee.java
@@ -0,0 +1,15 @@
+package org.java_lessons.lesson2;
+
+public class Employee {
+    String name;
+    int age;
+    double salary;
+
+    public Employee() {}
+
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
---
+++ Java_lessons/src/main/java/org/java_lessons/lesson2/Main.java
@@ -0,0 +1,32 @@
+package org.java_lessons.lesson2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //> < >= <= !=
+        int num1 = 10;
+        int num2 = 20;
+        boolean res = num1 < num2;
+        //System.out.println(res);
+        // И || | +
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = b1||b2; // = true
+        //           1 * 0 = 0
+        boolean b4 = b1 && b2;
+        boolean b5 = !b1;
+        //           1    0 = 1
+        boolean b6 = b1 ^ b2;
+
+//        Scanner sc = new Scanner(System.in);
+//        String number = sc.nextLine();
+//        Integer integer = Integer.valueOf(number);
+//        System.out.println("Парное число = " + (integer % 2 == 0 ? "парное" : "непарное"));
+
+        // ++ -- += -= *= /= %=
+        System.out.println(num1++);
+        System.out.println(num1);
+
+    }
+}
