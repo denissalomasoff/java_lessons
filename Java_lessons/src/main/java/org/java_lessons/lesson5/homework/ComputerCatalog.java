1d12364dcbdcd62864349a1b8b7f93f63da59621
Денис_Денис <denissalomasoff@gmail.com>
2026-02-14 17:37:52 +0300

    2 Часть ДЗ. методы equlals(), toString(), hashCode(). Исключение id из сравнения

--- Java_lessons/src/main/java/org/java_lessons/lesson5/homework/ComputerCatalog.java
+++ Java_lessons/src/main/java/org/java_lessons/lesson5/homework/ComputerCatalog.java
@@ -2,23 +2,23 @@ package org.java_lessons.lesson5.homework;
 
 public class ComputerCatalog {
     public static void main(String[] args) {
-        final Computer Computer1 = new Computer(123, "Dual-Core", 8, "NVidea RX7", 1200);
-        final Computer Computer2 = new Computer(3214, "Dual-Core", 8, "NVidea RX7", 1200);
-        final Computer Computer3 = new Computer(456456, "Quad-core", 12, "Yamaha R1", 1800);
-        final Computer Computer4 = new Computer(276, "Quad-core", 12, "Yamaha R1", 1800);
-        final Computer Computer5 = new Computer(163, "Octa-core", 24, "Radeon 300BestUltra", 2150);
+        final Computer сomputer1 = new Computer(123, "Dual-Core", 8, "NVidea RX7", 1200);
+        final Computer сomputer2 = new Computer(3214, "Dual-Core", 8, "NVidea RX7", 1200);
+        final Computer сomputer3 = new Computer(456456, "Quad-core", 12, "Yamaha R1", 1800);
+        final Computer сomputer4 = new Computer(276, "Quad-core", 12, "Yamaha R1", 1800);
+        final Computer сomputer5 = new Computer(163, "Octa-core", 24, "Radeon 300BestUltra", 2150);
 
         //Рефлексивность: x.equals(x) должен возвращать true
-        System.out.println("Рефлексивность " + Computer1.equals(Computer1)); //возвращает true
-        //Симметричность: если Computer1.equals(Computer2) возвращает true, то Computer2.equals(Computer1) тоже должен возвращать true;
-        System.out.print("Симметричность " + Computer1.equals(Computer2) + " ");
-        System.out.println(Computer2.equals(Computer1)); //true true
-        //Консистентность: повторные вызовы Computer1.equals(Computer3) должны возвращать одинаковый результат, если объекты не изменились
-        System.out.println("Консистентность " + Computer1.equals(Computer2));
+        System.out.println("Рефлексивность " + сomputer1.equals(сomputer1)); //возвращает true
+        //Симметричность: если сomputer1.equals(сomputer2) возвращает true, то сomputer2.equals(сomputer1) тоже должен возвращать true;
+        System.out.print("Симметричность " + сomputer1.equals(сomputer2) + " ");
+        System.out.println(сomputer2.equals(сomputer1)); //true true
+        //Консистентность: повторные вызовы сomputer1.equals(сomputer3) должны возвращать одинаковый результат, если объекты не изменились
+        System.out.println("Консистентность " + сomputer1.equals(сomputer2));
         //Cравнение hashCode на long
-        System.out.println("Хэш 1 " + (Computer1.hashCode()));
-        System.out.println("Хэш 2 " + (Computer2.hashCode()));
-        System.out.println("Сравнение hashCode " + (Computer1.hashCode() == Computer2.hashCode()));
+        System.out.println("Хэш 1 " + (сomputer1.hashCode()));
+        System.out.println("Хэш 2 " + (сomputer2.hashCode()));
+        System.out.println("Сравнение hashCode " + (сomputer1.hashCode() == сomputer2.hashCode()));
 
     }
 }
