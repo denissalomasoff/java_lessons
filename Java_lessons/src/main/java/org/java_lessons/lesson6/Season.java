d27a6ccebcb28fa2dedf3b0d21d9cdd82b3a35b5
Денис_Денис <denissalomasoff@gmail.com>
2026-02-17 08:41:20 +0300

    Времена года

---
+++ Java_lessons/src/main/java/org/java_lessons/lesson6/Season.java
@@ -0,0 +1,18 @@
+package org.java_lessons.lesson6;
+
+public enum Season {
+    WINTER("Зима"),
+    SPRING("Весна"),
+    SUMMER("Лето"),
+    FALL("Осень");
+    private final String rusName;;
+
+    Season(String rusName) {
+        this.rusName = rusName;
+    }
+
+    public String getRusName() {
+        return rusName;
+    }
+}
+
---
+++ Java_lessons/src/main/java/org/java_lessons/lesson6/WhichSeason.java
@@ -0,0 +1,54 @@
+package org.java_lessons.lesson6;
+
+import java.time.LocalDate;
+
+public class WhichSeason {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        String convertDate = date.toString();
+        char char1 = convertDate.charAt(5);
+        char char2 = convertDate.charAt(6);
+        //Season = Season.FALL;
+
+        System.out.println(convertDate);
+        System.out.println(char1 + " "+ char2);
+
+        if ((char1 == 0) && (char2 == 1)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Зима");
+        } else if ((char1 == 0) && (char2 == 2)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Зима");
+        } else if ((char1 == 0) && (char2 == 3)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Весна");
+        } else if ((char1 == 0) && (char2 == 4)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Весна");
+        } else if ((char1 == 0) && (char2 == 5)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Весна");
+        } else if ((char1 == 0) && (char2 == 6)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Лето");
+        } else if ((char1 == 0) && (char2 == 7)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Лето");
+        } else if ((char1 == 0) && (char2 == 8)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Лето");
+        } else if ((char1 == 0) && (char2 == 9)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Осень");
+        } else if ((char1 == 1) && (char2 == 0)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Осень");
+        } else if ((char1 == 1) && (char2 == 1)) {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Осень");
+        } else {
+            System.out.println("Текущая дата: " + date);
+            System.out.println("Время года: " + "Зима");
+        }
+    }
+}
