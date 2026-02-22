3495f8e0d7aaf911ca38e5e3c67304b72051abb8
Денис_Денис <denissalomasoff@gmail.com>
2026-02-22 11:51:41 +0300

    PhoneValidator

--- Java_lessons/src/main/java/org/java_lessons/lesson7/homework/PhoneValidator.java
+++ Java_lessons/src/main/java/org/java_lessons/lesson7/homework/PhoneValidator.java
@@ -3,32 +3,28 @@ package org.java_lessons.lesson7.homework;
 import java.util.Scanner;
 
 public class PhoneValidator {
-    final String number;
-
-    public PhoneValidator(String number) {
-        this.number = number;
-    }
-//       public boolean validate(String splitNumber){
-//           if (splitNumber.isEmpty()) {
-//                return false;
-//           }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите номер телефона");
-        String number = sc.nextLine().trim().replaceAll("[a-z\\s\\-_]", "");// убираем символы кроме цифр
+    // метод принимает на вход строку с любым наименованием (рандомНаймСтринг) - любое название переменной
+    public boolean validate(String randomNameString) {
+        boolean result = false;
+        if (randomNameString == null || randomNameString.isEmpty()) {
+            System.out.println("Строка пустая");
+            return result;
+        }
+        final String number = randomNameString.trim().replaceAll("[a-z\\s\\-_]", "");// убираем символы кроме цифр
         final String[] splitNumber = number.split("");// после удаления и склейки цифр из номера, разбиваем строку на символы
 
-        if (number.isEmpty()) {
-            System.out.println("Номер не введен, повторите попытку.");
-        } else if (((splitNumber[0].equals("+")) && (splitNumber.length == 12)) ||
-            (splitNumber[0].equals("7")) && (splitNumber.length == 11) ||
-            (splitNumber[0].equals("8") && (splitNumber.length == 11))) {
+        if (((splitNumber[0].equals("+")) && (splitNumber.length == 12)) ||
+                (splitNumber[0].equals("7")) && (splitNumber.length == 11) ||
+                (splitNumber[0].equals("8") && (splitNumber.length == 11))) {
             System.out.println("Номер принадлежит российскому оператору");
+            result = true;
         } else if (number.length() > 20) {
             System.out.println("Номер содержит более 20 символов, повторите ввод");
+            result = false;
         } else {
             System.out.println("Номер НЕ ПРИНАДЛЕЖИТ российскому оператору");
+            result = false;
         }
+        return result;
     }
 }
