1473ea6e7359b37adb71c89d5d6cc34a952d92f6
Денис_Денис <denissalomasoff@gmail.com>
2026-03-03 22:25:07 +0300

    Анаграммы

--- Java_lessons/src/main/java/org/java_lessons/lesson10/homework/WordsListExecutor.java
+++ Java_lessons/src/main/java/org/java_lessons/lesson10/homework/WordsListExecutor.java
@@ -14,18 +14,17 @@ public class WordsListExecutor {
         String word2 = scanner.nextLine();
 
         if (word1.isEmpty() || word2.isEmpty()) {
-            System.out.println("Одно, или второе, или оба два слова не введены");
+            System.out.println("Одно, или второе, или оба два слова не введены =)");
         } else if (word1.equals(word2)) {
-            System.out.println("Слова эквивалентны");
+            System.out.println("Слова эквивалентны.");
         } else if (word1.length() != word2.length()) {
-            System.out.println("Слова имеют разную длину");
+            System.out.println("Слова имеют разную длину.");
         } else if (AnargammCheck.Anagram(word1, word2)) {
-            System.out.println("Анаграммы");
+            System.out.println("Слова - анаграммы.");
         } else {
-            System.out.println("Не анаграммы, но имеют одинаковую длину");
+            System.out.println("Слова имеют одинаковую длину, но не являются анаграммами.");
         }
     }
 
-        //scanner.close();
+    //scanner.close();
 }
-
