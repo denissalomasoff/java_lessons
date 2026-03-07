1da76b48addd797451484c46d38b3033821329a5
Денис_Денис <denissalomasoff@gmail.com>
2026-03-07 17:01:50 +0300

    Дз

--- Java_lessons/src/main/java/org/java_lessons/lesson11/homework/CcpMain.java
+++ Java_lessons/src/main/java/org/java_lessons/lesson11/homework/CcpMain.java
@@ -1,7 +1,33 @@
 package org.java_lessons.lesson11.homework;
 
+import java.time.LocalDate;
+import java.util.UUID;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class CcpMain {
     public static void main(String[] args) {
 
+        final CorporateProductCredit credit1 = new CorporateProductCredit(UUID.randomUUID(), 3L, 2L, true, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit2 = new CorporateProductCredit(UUID.randomUUID(), 2L, 4L, false, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit3 = new CorporateProductCredit(UUID.randomUUID(), 1L, 5L, true, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit4 = new CorporateProductCredit(UUID.randomUUID(), 3L, 2L, false, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit5 = new CorporateProductCredit(UUID.randomUUID(), 4L, 1L, true, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit6 = new CorporateProductCredit(UUID.randomUUID(), 3L, 2L, true, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit7 = new CorporateProductCredit(UUID.randomUUID(), 1L, 3L, false, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit8 = new CorporateProductCredit(UUID.randomUUID(), 1L, 4L, true, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit9 = new CorporateProductCredit(UUID.randomUUID(), 3L, 5L, false, generateRandomDate(), generateRandomDate());
+        final CorporateProductCredit credit10 = new CorporateProductCredit(UUID.randomUUID(), 3L, 2L, true, generateRandomDate(), generateRandomDate());
+        System.out.println(credit8);
+        System.out.println(credit9);
+    }
+    private static LocalDate generateRandomDate() {
+        LocalDate startDate = LocalDate.of(2000, 1, 1);
+        LocalDate endDate = LocalDate.now();
+
+        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+        long randomDays = ThreadLocalRandom.current().nextLong(daysBetween + 1);
+
+        return startDate.plusDays(randomDays);
     }
 }
--- Java_lessons/src/main/java/org/java_lessons/lesson11/homework/CorporateProductCreditComparator.java
+++ Java_lessons/src/main/java/org/java_lessons/lesson11/homework/CorporateProductCreditComparator.java
@@ -1,17 +1,16 @@
-package org.java_lessons.lesson11.homework;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Comparator;
-
-public class CorporateProductCreditComparator implements Comparator {
-    public int compareCcp(@NotNull CorporateProductCredit o1, CorporateProductCredit o2) {
-        if (o1.getExpirationTime().isAfter(o2.getExpirationTime())) {
-            return -1;
-        } else if (o1.getExpirationTime().isBefore(o2.getExpirationTime())) {
-            return 1;
-        }
-        return 0;
-    }
-}
+//package org.java_lessons.lesson11.homework;
+//
+//import java.util.Comparator;
+//
+//@Override
+//public class CorporateProductCreditComparator implements Comparator {
+//    public int compareCcp( CorporateProductCredit o1, CorporateProductCredit o2) {
+//        if (o1.getExpirationTime().isAfter(o2.getExpirationTime())) {
+//            return -1;
+//        } else if (o1.getExpirationTime().isBefore(o2.getExpirationTime())) {
+//            return 1;
+//        }
+//        return 0;
+//    }
+//}
 
